1e6f429278c194897bd98da74ca79cb94a5f8b44
valentino3112 <valentinogoncalves12@gmail.com>
2023-11-26 17:38:29 +0100

    fonction extraire_nom_president

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def list_of_files(directory, extension): #on prends une entrée (directory) ains
         if filename.endswith(extension): #verifie si le nom du fichier se finit par la même extension
             files_names.append(filename) #si oui, il l'ajoute au fichier "files_names"
     return files_names
+
+#On récupère le nom du président et nous retourne le resultat
+def extraire_nom_president(nom: str):
+    result = nom[11:-4].rstrip("1234567890") #on prends le nom qui se trouve à l'indice 11 jusqu'à l'indice -4 en supprimant les chiffres allant de 0 à 9
+    return result
