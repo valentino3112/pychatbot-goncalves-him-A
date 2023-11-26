127e0467def057f99fd62dd3db5be2227ac3d4e4
Roxigers <james.him@efrei.net>
2023-11-26 17:39:29 +0100

    création def liste_des_pres

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def list_of_files(directory, extension): #on prends une entrée (directory) ains
 def extraire_nom_president(nom: str):
     result = nom[11:-4].rstrip("1234567890") #on prends le nom qui se trouve à l'indice 11 jusqu'à l'indice -4 en supprimant les chiffres allant de 0 à 9
     return result
+
+#On prends les noms des présidents sans doublons et le retourne dans la liste
+def liste_des_pres():
+    resultat = []
+    for txt in list_of_files("speeches", "txt"): #on choisie l'entrée ainsi que l'extension du fichier
+        if extraire_nom_president(txt) not in resultat: #si le nom du président n'est pas déjà dans le résultat, on le rajoute à la fin de la list
+            resultat.append(extraire_nom_president(txt))
+    return resultat
